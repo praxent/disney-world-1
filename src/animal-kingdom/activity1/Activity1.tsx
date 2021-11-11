22ebec2b4bd1b6ad4b8958bec5a57b7bec04308d
Edgar Torres <torres.edgar@gmail.com>
2021-11-11 12:28:07 -0600

    feat: Add functionality for activity 1

--- src/animal-kingdom/activity1/Activity1.tsx
+++ src/animal-kingdom/activity1/Activity1.tsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from "styled-components";
 
+import target from "./images/target.png";
+import splash from "./images/red_splash.png";
+
 const Styles = styled.div`
-  color: red;
-  
-  .container {
-    max-width: 900px;
-    margin: 0 auto;
+  color: Blue;
+
+  .container{
+    width:900px;
+    margin: 0 auto; 
+    text-align:center;
+    position:relative;
+}
+ 
+  .target {
+    height: 400px;
+    width: 400px;
+    display:block;
+    margin:auto;
+    position:relative;
+  }
+
+  .splash {
+    height: 45px;
+    width: 45px;
+    position:absolute;
+    float: center;
+    margin:auto;
+    top: 245px;
+    left: 430px;
   }
 `;
-
+ 
 function Activity1() {
+  const [visible, setVisible] = React.useState(false);
   return (
+
     <Styles>
       <div className="container">
-        Hello from Activity 1 Animal Kingdom
+        <h1>Activity 1</h1>
+        <div>
+          <img className="target" src={target} alt="target" />
+          {visible && <img className="splash" src={splash} alt="splash" />}
+        </div>
+        <div>
+          <button onClick={() => setVisible(!visible)}>
+            {visible ? 'Shoot' : 'Shoot'}
+          </button>
+
+        </div>
       </div>
     </Styles>
   )
